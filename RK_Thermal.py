2c209112c2888fe5a5edea2ad2959e45031cb016
Takumario <wonder.trip.634@gmail.com>
2017-06-12 14:42:15 +0900

    Cal_Mtlx completed

--- RK_Thermal.py
+++ RK_Thermal.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -13,6 +14,21 @@ c2 = 10  #Damping Coefficient [Ns/mm]
 Z20 = 0.0  #Initial Position of Pad [mm]
 DZ = 50    #Initial Deflextion of Bias Spring [mm]
 h = 0.1   #Interval of RK
+d = 1    #Diameter of SMA wire [mm]
+D = 6.8  #Diameter of SMA coil [mm]
+n = 10   #Number of coil spring
+rho = 6.5*10-3 #Density of SMA wire [g/mm^3]
+r = math.sqrt(math.pi*d*D*n/4) #radius of pseudo sphere of SMA[mm]
+A = math.pi*r**2 #Cross section of the pseudo sphere of SMA[mm^2]
+AA = 4*math.pi*r**2 #Surface are of SMA pseudo sphere [mm^2]
+Theta_L = 45  #Lattitude [deg]
+m = rho*(math.pi*(0.5*d))**2*D*n/1000 #Mass of a SMA spring [g]
+c = 440*10**-3  #Specific heat capacity of SMA spring [J/gK]
+Sc = 1366*10**-6 #Solar constant [W/mm^2]
+delta = 5.67*10**-2 #Stefan-Boltzman constant [W/mm^2K^4]
+a = 0.07 #Albedo constant
+Tg = 50 + 273 #Temperature of the ground[K]
+epsilon = 1.0 #Emissivity of the ground
 
 def func1(x):
     return [x[1], (k1/Mb)*(l0-(x[0]-x[2]))-g, x[3], (k1/Mp)*(l0-(x[0]-x[2])\
@@ -22,11 +38,15 @@ def func2(x, t):
     return [x[1], (k1/Mb)*(l0-(x[0]-x[2]))-g, x[3], (k1/Mp)*(l0-(x[0]-x[2])\
 	)-(k2/Mp)*(x[2]-Z20)-g]
 	
-def motion_test(x):
-	return np.array([x[2],x[1],x[0]])
+#def motion_test(x):
+	#return np.array([x[2],x[1],x[0]])
 
 def motion_test(x):
 	return np.array([x[1],-g])
+
+def ThermalEq(x):
+	q = A*math.cos(math.radians(Theta_L))*Sc/(m*c) + a*A*math.cos(math.radians(Theta_L))*Sc/(m*c) +epsilon*delta*A/(m*c)*(Tg**4-x[0]**4)/2 - delta*AA/(m*c)*(x[0]**4-4**4)
+	return q
 
 def RK(x):
 	k1 = motion_test(x)
@@ -36,6 +56,20 @@ def RK(x):
 	x_ = x + (h/6)*(k1+2*k2+2*k3+k4)
 #	print(x_)
 	return x_
+	
+def Cal_Mtlx(X0, t_s, t_f, l):
+	XX = np.empty((0,l), float)
+	XX = np.append(XX, np.array([[X0[0], X0[1]]]), axis=0)
+	t = t_s
+	n = 0
+	while(t<t_f):
+			Step = RK(XX[n])
+			S = np.array([[Step[0],Step[1]]])
+			XX = np.append(XX, S, axis=0)
+			t = t+h
+			n = n+1
+	
+	return XX
 
 def main():
 	t_s = 0.0
@@ -45,11 +79,13 @@ def main():
 	m = 1
 	v0 = 10
 	H = 1
+	X0 = [H,v0]
+	XX = Cal_Mtlx(X0, t_s, t_f, 2)
 	#Time = np.empty((0), float)
 	#Time = np.append(Time, np.array([[xH,v0]]), axis=0)
 	Time = []
-	XX = np.empty((0,2), float)
-	XX = np.append(XX, np.array([[H,v0]]), axis=0)
+#	XX = np.empty((0,2), float)
+#	XX = np.append(XX, np.array([[H,v0]]), axis=0)
 #	XX = np.append(XX, np.array([[4,5,6]]), axis=0)
 	print(XX)
 	#print(XX[1])
@@ -62,7 +98,7 @@ def main():
 	#print(XX)
 	#XX(0,:) = [0,0,0,0] 
 	#B = np.array([1,1,1,1])
-	while (t < t_f):
+#	while (t < t_f):
 	#	k1 = motion_test(XX[n])
 	#	k1 = np.array([3.0,2.0,1.0])
 #		print(k1)
@@ -72,14 +108,14 @@ def main():
 	#	k4 = motion_test(XX[n]+h*k3)
 	#	Step = XX[n] + (h/6)*(k1+2*k2+2*k3+k4)
 #		print(Step)
-		Step = RK(XX[n])
-		S = np.array([[Step[0],Step[1]]])
+#		Step = RK(XX[n])
+#		S = np.array([[Step[0],Step[1]]])
 #		A = np.array([[n,n,n]])
-		XX = np.append(XX, S, axis=0)
+#		XX = np.append(XX, S, axis=0)
 		#X(n+1,:) = XX(n,:) + B 
 #		Time = Time + [t]
-		t = t+h
-		n = n+1
+#		t = t+h
+#		n = n+1
 
 	print(n)
 	print(t)
