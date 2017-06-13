bdedeb2bd9cfac4f30cb6644cd827bbf071e5ec6
Takumario <wonder.trip.634@gmail.com>
2017-06-13 13:08:16 +0900

    OK

--- RK_Thermal.py
+++ RK_Thermal.py
@@ -17,27 +17,38 @@ h = 0.1   #Interval of RK
 d = 1    #Diameter of SMA wire [mm]
 D = 6.8  #Diameter of SMA coil [mm]
 n = 10   #Number of coil spring
-#rho = 6.5*10-3 #Density of SMA wire [g/mm^3]
-rho = 1
+rho = 6.5*10**-3 #Density of SMA wire [g/mm^3]
+print("Density of SMA wire [g/mm^3]")
+print(rho)
+#rho = 1
 r = math.sqrt(math.pi*d*D*n/4) #radius of pseudo sphere of SMA[mm]
-#A = math.pi*r**2 #Cross section of the pseudo sphere of SMA[mm^2]
-A = 1
-#AA = 4*math.pi*r**2 #Surface are of SMA pseudo sphere [mm^2]
-AA = 1
+print("radius of pseudo sphere of SMA[mm]")
+print(r)
+A = math.pi*r**2 #Cross section of the pseudo sphere of SMA[mm^2]
+print("Cross section of SMA pseudo sphere [mm^2]")
+print(A)
+#A = 1
+AA = 4*math.pi*r**2 #Surface are of SMA pseudo sphere [mm^2]
+print("Surface area of SMA pseudo sphere [mm^2]")
+print(AA)
+#AA = 1
 Theta_L = 45  #Lattitude [deg]
-#m = rho*(math.pi*(0.5*d))**2*D*n/1000 #Mass of a SMA spring [g]
-m = 1
-#c = 440*10**-3  #Specific heat capacity of SMA spring [J/gK]
-c = 500 
-#Sc = 1366*10**-6 #Solar constant [W/mm^2]
-Sc = 1
-#delta = 5.67*10**-2 #Stefan-Boltzman constant [W/mm^2K^4]
-delta = 1
-#a = 0.07 #Albedo constant
-a = 1
-#Tg = 50 + 273 #Temperature of the ground[K]
-Tg = 1
-epsilon = 1.0 #Emissivity of the ground
+m = rho*(math.pi*(0.5*d))**2*D*n #Mass of a SMA spring [g]
+print("Mass of SMA[g]")
+print(m)
+#m = 1
+c = 440*10**-3  #Specific heat capacity of SMA spring [J/gK]
+#c = 500 
+Sc = 1366*10**-6 #Solar constant [W/mm^2]
+#Sc = 1
+delta = 5.67*10**-2 #Stefan-Boltzman constant [W/mm^2K^4]
+#delta = 1
+a = 0.07 #Albedo constant
+#a = 1
+Tg = 100 + 273 #Temperature of the ground[K]
+#Tg = 1
+epsilon = 0.6 #Emissivity of the ground
+TB = 50 + 273
 
 def func1(x):
     return [x[1], (k1/Mb)*(l0-(x[0]-x[2]))-g, x[3], (k1/Mp)*(l0-(x[0]-x[2])\
@@ -53,54 +64,93 @@ def func2(x, t):
 def motion_test(x):
 	return np.array([x[1],-g])
 
-def ThermalEq(x):
+def ThermalEq1(x): #where Tg >= Tb
 	value1 = A*math.cos(math.radians(Theta_L))*Sc/(m*c)
 	print("value1")
 	print(value1)
 	value2 = a*A*math.cos(math.radians(Theta_L))*Sc/(m*c)
 	print("value2")
 	print(value2)
-	value3 = epsilon*delta*A*(Tg**4-x[0]**4)/(2*m*c)
+	value3 = epsilon*delta*A*((Tg/TB)**4-(x[0]/TB)**4)/(2*m*c)
+	print"(Tg/TB)**4 = {0}".format((Tg/TB)**4) 
+	print"(Tb/TB)**4 = {0}".format((x[0]/TB)**4)
 	print("value3")
 	print(value3)
-	value4 = delta*AA*(x[0]**4-4**4)/(m*c)
+	value4 = delta*AA*((x[0]/TB)**4-(4/TB)**4)/(m*c)
 	print("value4")
 	print(value4)
 	q = value1 + value2 + value3 - value4 
+#	q = value1 + value2 
 	return q
+
+def ThermalEq2(x):   #where Tb > Tg
+	value1 = A*math.cos(math.radians(Theta_L))*Sc/(m*c)
+	print("value1")
+	print(value1)
+	value2 = a*A*math.cos(math.radians(Theta_L))*Sc/(m*c)
+	print("value2")
+	print(value2)
+	value3 = delta*A*((x[0]/TB)**4 - (Tg/TB)**4)/(2*m*c)
+	print("value3")
+	print(value3)
+	value4 = delta*AA*((x[0]/TB)**4-(4/TB)**4)/(m*c)
+	print("value4")
+	print(value4)
+	q = value1 + value2 - value3 - value4 
+	#q = value1 + value2 
+	return q
+
 '''	
-def RK(x):
+def RK(x):  
 	k1 = motion_test(x)
 	k2 = motion_test(x+0.5*h*k1)
 	k3 = motion_test(x+0.5*h*k2)
 	k4 = motion_test(x+h*k3)
 	x_ = x + (h/6)*(k1+2*k2+2*k3+k4)
-	print(x_)
+	#print(x_)
 	return x_
 '''
-def RK(x):
-	k1 = ThermalEq(x)
-	k2 = ThermalEq(x+0.5*h*k1)
-	k3 = ThermalEq(x+0.5*h*k2)
-	k4 = ThermalEq(x+h*k3)
+def RK1(x): #Where Tg >= Tb 
+	k1 = ThermalEq1(x)
+	k2 = ThermalEq1(x+0.5*h*k1)
+	k3 = ThermalEq1(x+0.5*h*k2)
+	k4 = ThermalEq1(x+h*k3)
 	x_ = x + (h/6)*(k1+2*k2+2*k3+k4)
 	#print(x_)
 	return x_
 #'''
+def RK2(x): #Where Tb > Tg 
+	k1 = ThermalEq2(x)
+	k2 = ThermalEq2(x+0.5*h*k1)
+	k3 = ThermalEq2(x+0.5*h*k2)
+	k4 = ThermalEq2(x+h*k3)
+	x_ = x + (h/6)*(k1+2*k2+2*k3+k4)
+	#print(x_)
+	return x_
+
 def Cal_Mtlx(X0, t_s, t_f, l):
 	XX = np.empty((0,l), float)
 	XX = np.append(XX, np.array([[X0[0]]]), axis=0)
 	t = t_s
 	n = 0
 	while(t<t_f):
-			Step = RK(XX[n])
-			print(n)
-			S = np.array([[Step[0]]])
-			#S = np.array(Step)
-			XX = np.append(XX, S, axis=0)
-			t = t+h
-			n = n+1
-	
+			if X0[0] <= Tg:
+				Step = RK1(XX[n])
+				print(n)
+				S = np.array([[Step[0]]])
+				#S = np.array(Step)
+				XX = np.append(XX, S, axis=0)
+				t = t+h
+				n = n+1
+			else:
+				Step = RK2(XX[n])
+				print(n)
+				S = np.array([[Step[0]]])
+				#S = np.array(Step)
+				XX = np.append(XX, S, axis=0)
+				t = t+h
+				n = n+1
+
 	return (XX)
 
 def main():
@@ -112,8 +162,8 @@ def main():
 	v0 = 10
 	H = 1
 	#X0 = [H,v0]
-	X0 = [1]
-	#print(X0)
+	X0 = [(80+273)/TB]
+	print(X0)
 	#print("This is for test")
 #	print(RK(X0))
 #	print(RK(X0)[1])
@@ -124,7 +174,7 @@ def main():
 #	XX = np.empty((0,2), float)
 #	XX = np.append(XX, np.array([[H,v0]]), axis=0)
 #	XX = np.append(XX, np.array([[4,5,6]]), axis=0)
-	print(XX)
+#	print(XX)
 	#print(XX[1])
 	#XX = ([0,0,0,0])
 	#XX.append([1,1,1,1])
@@ -156,7 +206,8 @@ def main():
 
 	#print(n)
 	#print(t)
-#	print(XX)
+	XX = TB*XX
+	print(XX)
 #	print(Time)
 	T = np.arange(0, t_f+2*h, h)
 	print(T)
---
+++ Test.py
@@ -0,0 +1,9 @@
+import math
+
+d=1.0
+D=5.8
+n=15
+rho=6.5*10**-3
+m = math.pi**2 * (0.5*d)**2 * D* n * rho
+print("Mass")
+print(m)
