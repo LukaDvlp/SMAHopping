2434264e6ba9578758b20ca2a6e2ec4dd60e100f
Takumario <wonder.trip.634@gmail.com>
2017-07-14 15:59:12 +0900

    add I in Thermal_Plate.py

--- Thermal_Plate.py
+++ Thermal_Plate.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 h = 0.1   #Interval of RK
-#d = 1    #Diameter of SMA wire [mm]
 d = 0.001    #Diameter of SMA wire [m]
 D = 0.0068  #Diameter of SMA coil [m]
 n = 10   #Number of coil spring
@@ -24,28 +23,18 @@ print(AA)
 #AA = 1
 Theta_L = 45  #Lattitude [deg]
 m = number*rho*(math.pi*(0.5*d))**2*D*n #Mass of a SMA spring [kg]
-#m = 0.003 
-#c = 440  #Specific heat capacity of SMA spring [J/kg K]
 c = 440  #Specific heat capacity of SMA spring [J/kg K]
 Sc = 1366 #Solar constant [W/m^2]
-#Sc = 1
 delta = 5.67*10**-8 #Stefan-Boltzman constant [W/m^2K^4]
-#delta = 1
 a = 0.07 #Albedo constant
-#a = 1
-#Tg = 120 + 273.0 #Temperature of the ground[K]
-#Tg = 1
 epsilon = 0.95 #Emissivity of the ground
-#ThetaB = 1
-#epsilonB = 1
 deltaB = 1
-#Sb = 0.02*0.02  #Area of heat sink [m^2] 
 Sb = 0.03*0.03  #Area of heat sink [m^2] 
 theta = 45 #angle of hopping leg 
 Fb_g = 0.5*(1+math.cos(math.radians(theta)))
 Fb_s = 0.5*(1-math.cos(math.radians(theta)))
 alpha = 1.0 #Emissivity of heat sink
-
+R = 1 #Resistance of SMA wire
 
 def ThermalEq1(x): #where Tg >= Tb
 	value1 = A*math.cos(math.radians(Theta_L))*Sc/(m*c)
@@ -61,7 +50,6 @@ def ThermalEq1(x): #where Tg >= Tb
 	print("value3")
 	print(value3)
 	value4 = delta*AA*(x[0]**4-4**4)/(m*c)
-#	value4 = delta*A*((x[0]/TB)**4-(4/TB)**4)/(m*c)
 	print("value4")
 	print(value4)
 	q = value1 + value2 + value3 - value4 
@@ -86,20 +74,24 @@ def ThermalEq2(x):   #where Tb > Tg
 	q = value1 - value3 - value4
 	return q
 
-def T_Eq1(x, Tg):   #Where Tg>Tb, with plate
+def T_Eq1(x, Tg, I):   #Where Tg>Tb, with plate
 	value1 = Sb*Fb_g*epsilon*delta*(Tg**4-x[0]**4)/(2*m*c)
 	value2 = Sb*Fb_s*alpha*delta*(x[0]**4-4**4)/(m*c)
+	value3 = R*I**2/(m*c) 
 	print("value1={0}".format(value1))
 	print("value2={0}".format(value2))
-	q = value1-value2
+	print("value3={0}".format(value3))
+	q = value1-value2+value3
 	return q
 
-def T_Eq2(x, Tg):   #Where Tg>Tb, with plate
+def T_Eq2(x, Tg, I):   #Where Tb>Tg, with plate
 	value1 = Sb*Fb_g*alpha*delta*(x[0]**4-Tg**4)/(m*c)
 	value2 = Sb*Fb_s*alpha*delta*(x[0]**4-4**4)/(m*c)
+	value3 = R*I**2/(m*c) 
 	print("value1={0}".format(value1))
 	print("value2={0}".format(value2))
-	q = -value1-value2
+	print("value3={0}".format(value3))
+	q = -value1-value2+value3
 	return q
 
 def RK(x,f):  
@@ -111,20 +103,20 @@ def RK(x,f):
 	#print(x_)
 	return x_
 
-def RK1(x, Tg): #Where Tg >= Tb 
-	k1 = T_Eq1(x, Tg)
-	k2 = T_Eq1(x+0.5*h*k1, Tg)
-	k3 = T_Eq1(x+0.5*h*k2, Tg)
-	k4 = T_Eq1(x+h*k3,Tg)
+def RK1(x, Tg, I): #Where Tg >= Tb 
+	k1 = T_Eq1(x, Tg, I)
+	k2 = T_Eq1(x+0.5*h*k1, Tg, I)
+	k3 = T_Eq1(x+0.5*h*k2, Tg, I)
+	k4 = T_Eq1(x+h*k3,Tg, I)
 	x_ = x + (h/6)*(k1+2*k2+2*k3+k4)
 	#print(x_)
 	return x_
 
-def RK2(x, Tg): #Where Tb > Tg 
-	k1 = T_Eq2(x,Tg)
-	k2 = T_Eq2(x+0.5*h*k1,Tg)
-	k3 = T_Eq2(x+0.5*h*k2, Tg)
-	k4 = T_Eq2(x+h*k3, Tg)
+def RK2(x, Tg, I): #Where Tb > Tg 
+	k1 = T_Eq2(x,Tg, I)
+	k2 = T_Eq2(x+0.5*h*k1,Tg, I)
+	k3 = T_Eq2(x+0.5*h*k2, Tg, I)
+	k4 = T_Eq2(x+h*k3, Tg, I)
 	x_ = x + (h/6)*(k1+2*k2+2*k3+k4)
 	#print(x_)
 	return x_
@@ -136,21 +128,39 @@ def Cal_Mtlx(X0, t_s, t_f, l, Tg):
 	n = 0
 	while(t<t_f):
 			if XX[n] <= Tg:
-				Step = RK1(XX[n], Tg)
-				print(n)
-				S = np.array([[Step[0]]])
-				#S = np.array(Step)
-				XX = np.append(XX, S, axis=0)
-				t = t+h
-				n = n+1
+					if XX[n] < 80+273:
+							I = 2
+							Step = RK1(XX[n], Tg, I)		
+							print(n)
+							S = np.array([[Step[0]]])
+							XX = np.append(XX, S, axis=0)
+							t = t+h
+							n = n+1
+					else:	
+							I = 0
+							Step = RK1(XX[n], Tg, I)		
+							print(n)
+							S = np.array([[Step[0]]])
+							XX = np.append(XX, S, axis=0)
+							t = t+h
+							n = n+1
 			else:
-				Step = RK2(XX[n], Tg)
-				print(n)
-				S = np.array([[Step[0]]])
-				#S = np.array(Step)
-				XX = np.append(XX, S, axis=0)
-				t = t+h
-				n = n+1
+					if XX[n] < 80+273:
+							I =1 
+							Step = RK2(XX[n], Tg, I)		
+							print(n)
+							S = np.array([[Step[0]]])
+							XX = np.append(XX, S, axis=0)
+							t = t+h
+							n = n+1
+					else:	
+							I =0 
+							Step = RK2(XX[n], Tg, I)		
+							print(n)
+							S = np.array([[Step[0]]])
+							XX = np.append(XX, S, axis=0)
+							t = t+h
+							n = n+1
 
 	return (XX)
 
@@ -159,37 +169,37 @@ def main():
 	t_f = 2000.0 
 	t = 0
 	n = 0
-	X0 = [80+273]
+	X0 = [20+273]
 	print(X0)
-	Tg = 120+273
-	XX1 = Cal_Mtlx(X0, t_s, t_f, 1, Tg)
-	Tg = 80+273
-	XX2 = Cal_Mtlx(X0, t_s, t_f, 1, Tg)
-	Tg = 50+273
-	XX3 = Cal_Mtlx(X0, t_s, t_f, 1, Tg)
-	Tg = 20+273
-	XX4 = Cal_Mtlx(X0, t_s, t_f, 1, Tg)
+#	Tg = 120+273
+#	XX1 = Cal_Mtlx(X0, t_s, t_f, 1, Tg)
+#	Tg = 80+273
+#	XX2 = Cal_Mtlx(X0, t_s, t_f, 1, Tg)
+#	Tg = 50+273
+#	XX3 = Cal_Mtlx(X0, t_s, t_f, 1, Tg)
+#	Tg = 20+273
+#	XX4 = Cal_Mtlx(X0, t_s, t_f, 1, Tg)
 	Tg = 0+273
 	XX5 = Cal_Mtlx(X0, t_s, t_f, 1, Tg)
 	Time = []
-	print(XX1)
-	print(XX2)
+	print(XX5)
+#	print(XX2)
 	T = np.arange(0, t_f+2*h, h)
 	#T = np.arange(0, t_f+h, h)
 	#T = 0.01*T
 	print(T)
 	print("mass of SMA={0}".format(m))
 	print("Length of T = {0}".format(len(T)))
-	print("size of XX1")
-	rows1, cols1 = XX1.shape
-	print(rows1,cols1)
-	print("size of XX2")
-	rows2, cols2 = XX2.shape
-	print(rows2,cols2)
-	plt.plot(T,XX1[:,0], label="Tg:393[K]")
-	plt.plot(T,XX2[:,0], label="Tg:353[K]")
-	plt.plot(T,XX3[:,0], label="Tg:323[K]")
-	plt.plot(T,XX4[:,0], label="Tg:293[K]")
+	print("size of XX5")
+	rows1, cols1 = XX5.shape
+	#print(rows1,cols1)
+	#print("size of XX2")
+	#rows2, cols2 = XX2.shape
+	#print(rows2,cols2)
+#	plt.plot(T,XX1[:,0], label="Tg:393[K]")
+#	plt.plot(T,XX2[:,0], label="Tg:353[K]")
+#	plt.plot(T,XX3[:,0], label="Tg:323[K]")
+#	plt.plot(T,XX4[:,0], label="Tg:293[K]")
 	plt.plot(T,XX5[:,0], label="Tg:273[K]")
 	plt.xlabel('Time [s]')
 	plt.ylabel('Temperature [s]')
