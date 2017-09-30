90db0577804037bd6538b53c02cf007bdd8cf60b
Takumario <wonder.trip.634@gmail.com>
2017-09-30 14:41:47 +0900

    before modify

--- Optimization.py
+++ Optimization.py
@@ -14,31 +14,29 @@ class Optimize():
 
 	def __init__(self):
 		self.N_init = 1
-		self.d_init = 5
-		self.D_init = 1
-		self.n_init = 2
-		self.k_init = 1
-		self.L_init = 2
+		self.d_init = 0.8
+		self.D_init = 5
+		self.n_init = 3
+		self.k_init = 0.8
+		self.L_init = 60
 		self.N = self.N_init
 		self.d = self.d_init
 		self.D = self.D_init
 		self.n = self.n_init
 		self.k = self.k_init
 		self.L = self.L_init
-		self.N_fin = 3 
-		self.d_fin = 3  
-		self.D_fin = 3 
-		self.n_fin = 3 
-		self.k_fin = 3 
-		self.L_fin = 3 
+		self.N_fin = 10 
+		self.d_fin = 1.5 
+		self.D_fin = 12
+		self.n_fin = 20
+		self.k_fin = 6.0 
+		self.L_fin = 200 
 		self.N_spn = 1
-		self.d_spn = 1
-		self.D_spn = 1
+		self.d_spn = 0.1
+		self.D_spn = 0.5
 		self.n_spn = 1
-		self.k_spn = 1
-		self.L_spn = 1
-		self.BL = 500   #Battery Limit
-		self.LL = 100   #Latch Limit
+		self.k_spn = 0.1
+		self.L_spn = 2
 		self.Result = np.empty((0, 7), float)
 		self.initialise()
 
@@ -89,23 +87,25 @@ class Optimize():
 		self.DEL_2 = (self.N*self.Gm*self.d**4/(8*self.D**3*self.n)*self.dl_A+self.k*self.L+self.N*np.pi*self.d**3*self.Gm*self.gamma_l/(8*self.D))/(self.N*self.Gm*self.d**4/(8*self.D**3*self.n)+self.k)
 		if self.DEL_1 < self.dl_1:
 				self.dl_M = (self.N*self.Gm*self.d**4/(8*self.D**3*self.n)*self.dl_A+self.k*self.L)/(self.N*self.Gm*self.d**4/(8*self.D**3*self.n)+self.k)
-				if CheckCondition():
-						E = EF1()
+				if self.CheckCondition():
+						E = self.EF1()
 				else:
 						E = 0
 				arr = [[self.N,self.d,self.D,self.n,self.k,self.L,E]]
+				print arr
 				self.Result = np.append(self.Result, np.array(arr), axis=0)
 				
 		elif self.dl_1 < self.DEL_1 and self.DEL_1 < self.dl_2:
 				F_s = self.N*np.pi*self.d**3*self.tau_s/(8*self.D)
 				F_f = self.N*np.pi*self.d**3*self.tau_f/(8*self.D)
-				self.dl_M = ((F_f-F_s)/(dl_2-dl_1)*dl_A -F_s +(F_f-F_s)/(dl_2-dl_1) + self.k*self.L)/((F_f-F_s)/(dl_2-dl_1)+self.k)
-				self.F1 = (F_f-F_s)/(dl_2-dl_1)*dl_A + F_s - (F_f-F_s)/(dl_2-dl_1)*dl_1
-				if CheckCondition():
-						E = EF2()
+				self.dl_M = ((F_f-F_s)/(self.dl_2-self.dl_1)*self.dl_A -F_s +(F_f-F_s)/(self.dl_2-self.dl_1) + self.k*self.L)/((F_f-F_s)/(self.dl_2-self.dl_1)+self.k)
+				self.F1 = (F_f-F_s)/(self.dl_2-self.dl_1)*self.dl_A + F_s - (F_f-F_s)/(self.dl_2-self.dl_1)*self.dl_1
+				if self.CheckCondition():
+						E = self.EF2()
 				else:
 						E = 0
 				arr = [[self.N,self.d,self.D,self.n,self.k,self.L,E]]
+				print arr
 				self.Result = np.append(self.Result, np.array(arr), axis=0)
 
 		elif self.dl_2 < self.DEL_2:
@@ -115,10 +115,13 @@ class Optimize():
 				self.F1 = -self.k*(self.dl_M-self.L)
 				self.F2 = self.N*np.pi*self.d**3*self.tau_f/(8*self.D)
 				self.F3 = (F_f-F_s)/(self.dl_2-self.dl_1)*self.dl_A + F_s - (F_f-F_s)/(self.dl_2-self.dl_1)*self.dl_1
-				if CheckCondition():
-						E = EF3()
+				if self.CheckCondition():
+						E = self.EF3()
 				else:
 						E = 0
+				arr = [[self.N,self.d,self.D,self.n,self.k,self.L,E]]
+				print arr
+				self.Result = np.append(self.Result, np.array(arr), axis=0)
 	
 	def get_max(self, Result):
 		index = Result[:,6].argmax()
@@ -126,13 +129,13 @@ class Optimize():
 
 	def initialise(self):
 		#Global constants
-		self.Ga = 18000
+		self.Ga = 16000
 		self.Gm = 6000
-		self.tau_s = 200
-		self.tau_f = 500
-		self.tau_ay = 200
-		self.tau_my = 500
-		self.gamma_l = 0.5
+		self.tau_s = 32.4 
+		self.tau_f = 74.0
+		self.tau_ay = 550
+		self.tau_my = 290
+		self.gamma_l = 0.05
 		self.rho = 6.5
 		self.c = 440
 		self.Af = 80
@@ -142,14 +145,14 @@ class Optimize():
 		self.C = self.Ga/8
 		self.F = np.pi*self.tau_s/8
 		#Constraint Conditions
-		self.Ltc_max = 100 #Maximum of Latch
-		self.Bat_max = 100   #Maximum of Battery
+		self.Ltc_max = 200 #Maximum of Latch
+		self.Bat_max = 4400   #Maximum of Battery
 	
 	def CheckCondition(self):
 		Latch = -self.k*(self.dl_A-self.L) < self.Ltc_max
-		Battery = 6*self.N*self.rho*(np.pi)**2*(0.5*d)**2*self.D*self.n/1000*self.c/1000*(self.Af-self.Ms) < self.Bat_max
+		Battery = 6*self.N*self.rho*(np.pi)**2*(0.5*self.d)**2*self.D*self.n/1000*self.c/1000*(self.Af-self.Ms) < self.Bat_max
 		SpringIndex = 5 < self.D/self.d < 12
-		Yelding = dl_A < dl_Ay and dl_M < dl_My
+		Yelding = self.dl_A < self.dl_Ay and self.dl_M < self.dl_My
 		MaxLength = np.pi*self.D*self.n > self.L
 		return Latch and Battery and SpringIndex and Yelding and MaxLength
 
